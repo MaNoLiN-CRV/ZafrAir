6070d5341eca445dfc3dbd913b9d1c31ca79b192
MaNoLiN-CRV <manoliin.27@gmail.com>
2025-02-03 18:21:03 +0100

    Bottom navigation added

---
+++ src/screens/BottomTab.tsx
@@ -0,0 +1,29 @@
+// Import React Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './HomeScreen';
+import Settings from './Settings';
+import { theme } from '../core/theme';
+
+
+const Tab = createBottomTabNavigator();
+
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.secondary,
+      }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
